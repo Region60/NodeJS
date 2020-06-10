09a24f3ecb5375cfaa072ab2349f9af163cc46bf
Region60 <_max_kot@mail.ru>
2020-06-10 18:01:14 +0300

    'bcryptjs'

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const User = require('../models/user')
 const router = Router()
+const bcrypt = require('bcryptjs')
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -16,15 +17,35 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('5edabb16d65e480840856f1c')
-    req.session.user = user     //добавляем пользователя
-    req.session.isAuthenticated = true        //.session добавилось помле установки express-session. TRUE если залогиненв
-    req.session.save(err =>{     // метод позволяет подождать пока выполнятся предыдущие два действия и потом выполнить
-        if(err){                     //редирект
-            throw err
+    try {
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email})  //ищем юзера по email
+
+        if (candidate) {
+/*            const areSame = password === candidate.password  //заносим переменную password со значением candidate.password*/
+            const areSame = await  bcrypt.compare(password, candidate.password) //сравниваем пароли
+            if (areSame) {
+                req.session.user = candidate     //добавляем пользователя
+                req.session.isAuthenticated = true        //.session добавилось помле установки express-session. TRUE если залогиненв
+                req.session.save(err => {     // метод позволяет подождать пока выполнятся предыдущие два действия и потом выполнить
+                    if (err) {                     //редирект
+                        throw err
+                    }
+                    res.redirect('/')
+                })
+            }else {
+                res.redirect('/auth/login#login')
+
+            }
+        } else {
+            res.redirect('/auth/login#login')
+
         }
-        res.redirect('/')
-    })
+    } catch (e) {
+        console.log(e)
+    }
+
 })
 
 router.post('/register', async (req, res) => {
@@ -35,15 +56,16 @@ router.post('/register', async (req, res) => {
         if (candidate) {
             res.redirect('/auth/login#register')
         } else {
+            const hashPassword =await bcrypt.hash(password, 10) //что кодируем, второй параметр сложность шифрования(10норм)
             const user = new User({
-                email,password,name, cart: {items:[]}
+                email, password: hashPassword, name, cart: {items: []}
             })
             console.log(user)
             await user.save()
             res.redirect('/auth/login#login')
         }
     } catch (e) {
-        console.log (e)
+        console.log(e)
     }
 })
 
