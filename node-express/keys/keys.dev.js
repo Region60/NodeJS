cb1cea3b75342b578b5f3dcf9966dc600d3ef097
Region60 <58218488+Region60@users.noreply.github.com>
2020-08-21 17:38:57 +0300

    Delete keys.dev.js

--- node-express/keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://maksim:8u2upvDe0W1dp945@cluster0-mjkka.mongodb.net/shop',
-    SESION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: '_max_kot@mail.ru',
-    BASE_URL: 'http://localhost:3000'
-}
